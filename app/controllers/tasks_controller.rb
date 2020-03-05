ff12272104ce885c5880c10cc16731e1aa6ab3cf
sawkun <saru931aaa@yahoo.co.jp>
2020-03-05 11:33:54 +0000

    tasks/index.html.erbの1行目に<h1><%= current_user.name %>のタスク一覧</h1>を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,26 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  
   
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(created_at: :desc)
   end
   
   def show
   end
   
   def new
+    @task = Task.new(name: params[:name], detail: params[:detail])
   end
   
   def create
-    redirect_to tasks_url
+    @task = Task.new(name: params[:name], detail: params[:detail])
+    if @task.save
+       flash[:success] = "タスク新規作成しました。"
+       redirect_to user_tasks_url
+    else
+      render :new
+    end
   end
   
   def edit
@@ -22,5 +31,11 @@ class TasksController < ApplicationController
   
   def destroy
   end
+  
+  private
+  
+   def set_user
+     @user = User.find(params[:user_id])
+   end
   
 end
