58cea90d47078dc9d58f514153b9fc3b69f9f95f
sawkun <saru931aaa@yahoo.co.jp>
2020-03-06 08:46:24 +0000

    タスク一覧ページに編集と削除ボタンの外枠だけ追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,14 +10,14 @@ class TasksController < ApplicationController
   end
   
   def new
-    @task = Task.new(name: params[:name], detail: params[:detail])
+    @task = Task.new
   end
   
   def create
-    @task = Task.new(name: params[:name], detail: params[:detail])
+    @task = @user.tasks.build(task_params)
     if @task.save
        flash[:success] = "タスク新規作成しました。"
-       redirect_to user_tasks_url
+       redirect_to user_tasks_url @user
     else
       render :new
     end
@@ -36,6 +36,10 @@ class TasksController < ApplicationController
   
    def set_user
      @user = User.find(params[:user_id])
+   end
+   
+   def task_params
+     params.require(:task).permit(:name, :detail, :created_at, :updated_at)
    end
   
 end
