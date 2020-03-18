2c576e64234356fb962df78fbd72ee972e95c8eb
sawkun <saru931aaa@yahoo.co.jp>
2020-03-18 00:00:26 +0000

    編集ページを作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = @user.tasks.build(task_params)
+    @task = Task.new(task_params)
     if @task.save
        flash[:success] = "タスク新規作成しました。"
        redirect_to user_tasks_url @user
